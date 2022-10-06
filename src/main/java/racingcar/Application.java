92cfe670428188506fdc4a86ef09e0bfd372e1a3
skylee273 <skylee273@koreatech.ac.kr>
2022-10-06 22:27:36 +0900

    feat(InputView) Add InputView.RacingCardInputNames

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,6 +4,6 @@ import racingcar.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
-        InputView.readyManual();
+        String[] cardNames = InputView.RacingCardInputNames();
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,13 +1,19 @@
 package racingcar.view;
 
+import racingcar.utils.ScannerUtil;
+
 import static racingcar.constant.RacingGameMessage.RACING_CAR_NAME_INPUT;
 
 public class InputView {
 
-    public static void readyManual(){
+    public static String[] RacingCardInputNames(){
         printMessage(RACING_CAR_NAME_INPUT);
+        return splitByComma(ScannerUtil.readLine());
     }
 
+    private static String[] splitByComma(String input) {
+        return input.split(",");
+    }
     private static void printMessage(String message) {
         System.out.println(message);
     }
